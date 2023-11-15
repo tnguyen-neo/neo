c51121f3290cb3b8f0ead7af6345982cb7df6c7f
Nguyen Tam Nguyen <ntamnguyen@tma.com.vn>
2023-11-15 11:12:19 +0700

    Set up static files & scss

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const hbs = require("express-handlebars");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.static(path.join(__dirname, "public")));
+
+// HTTP logger
+app.use(morgan("combined"));
+
+// Template engine
+app.engine("hbs", hbs.engine({ extname: ".hbs" }));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/news", (req, res) => {
+  res.render("news");
+});
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
+  console.log(`Listening on port ${port}`);
+});
